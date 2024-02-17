1e08685f952e7b16a0dc6cd3b0f00ebb6804595a
spidery804dice <garikmishkin@gmail.com>
2024-02-17 15:32:02 +0300

    Второе задание

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import pygame
+import requests
+
+
+class MapParams(object):
+    def __init__(self):
+        self.lat = 60.035131
+        self.lon = 30.381382
+        self.zoom = 16
+        self.type = "map"
+
+    def l_l(self):
+        return str(self.lon) + "," + str(self.lat)
+
+
+def load_map(map_):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=map_.l_l(), z=map_.zoom, type=map_.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса")
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    map_ = MapParams()
+    map_file = load_map(map_)
+    pygame.display.set_caption("Карта")
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PAGEUP:
+                    if map_.zoom < 19:
+                        map_.zoom += 1
+                elif event.key == pygame.K_PAGEDOWN:
+                    if map_.zoom > 0:
+                        map_.zoom -= 1
+
+        map_file = load_map(map_)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        clock.tick(50)
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
